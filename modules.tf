9c5696aa55f2803f698f70dede41b762612d6cf5
Luiz Scofield <luizscofield.pro@gmail.com>
2024-05-31 18:55:11 -0300

    fix: terraform fmt

--- modules.tf
+++ modules.tf
@@ -1,6 +1,6 @@
 module "eks-network" {
-  source = "./modules/network"
+  source         = "./modules/network"
   vpc_cidr_block = var.vpc_cidr_block
-  project_name = var.project_name
-  global_tags = local.global_tags
+  project_name   = var.project_name
+  global_tags    = local.global_tags
 }
