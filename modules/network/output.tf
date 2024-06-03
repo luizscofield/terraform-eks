ad5e199516729f0d7224e4205a95bf7762f2a08a
Luiz Scofield <luizscofield.pro@gmail.com>
2024-06-03 09:28:57 -0300

    fix: network output

--- modules/network/output.tf
+++ modules/network/output.tf
@@ -3,17 +3,17 @@ output "vpc_id" {
 }
 
 output "subnet-pub-a" {
-  value = aws_subnet.tf-eks-subnet-pub-1a
+  value = aws_subnet.tf-eks-subnet-pub-1a.id
 }
 
 output "subnet-pub-b" {
-  value = aws_subnet.tf-eks-subnet-pub-1b
+  value = aws_subnet.tf-eks-subnet-pub-1b.id
 }
 
 output "subnet-priv-a" {
-  value = aws_subnet.tf-eks-subnet-priv-1a
+  value = aws_subnet.tf-eks-subnet-priv-1a.id
 }
 
 output "subnet-priv-b" {
-  value = aws_subnet.tf-eks-subnet-priv-1b
+  value = aws_subnet.tf-eks-subnet-priv-1b.id
 }
