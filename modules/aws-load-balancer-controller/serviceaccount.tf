28ae0119ed717b6d2759d8eb0c177e3e3182183d
Luiz Scofield <luizscofield.pro@gmail.com>
2024-06-03 22:41:45 -0300

    feat: adding controller service account

---
+++ modules/aws-load-balancer-controller/serviceaccount.tf
@@ -0,0 +1,14 @@
+resource "kubernetes_service_account" "tf-eks-controller-service-account" {
+  metadata {
+    name      = "aws-load-balancer-controller"
+    namespace = "kube-system"
+    labels = {
+      "app.kubernetes.io/component" = "controller"
+      "app.kubernetes.io/name"      = "aws-load-balancer-controller"
+    }
+    annotations = {
+      "eks.amazonaws.com/role-arn" = aws_iam_role.tf-eks-controller-role.arn
+    }
+  }
+}
+
--- modules/eks-cluster/output.tf
+++ modules/eks-cluster/output.tf
@@ -5,3 +5,11 @@ output "cluster_name" {
 output "oidc" {
   value = aws_eks_cluster.tf-eks-cluster.identity[0].oidc[0].issuer
 }
+
+output "certificate_authority" {
+  value = aws_eks_cluster.tf-eks-cluster.certificate_authority
+}
+
+output "endpoint" {
+  value = aws_eks_cluster.tf-eks-cluster.endpoint
+}
